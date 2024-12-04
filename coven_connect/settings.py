3c79e4a123b3a23837f4c570cfa2943852cbe7f5
Eva Juez <xv4w04bu@students.codeinstitute.net>
2024-12-04 11:43:18 +0000

    Add secret key

--- coven_connect/settings.py
+++ coven_connect/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0-&983m1@u&ju$9^itj_7q9wto*ral10_gfjno&=o06)m5-xh)'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
