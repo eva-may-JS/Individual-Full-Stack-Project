9da8d5ae6145f2c5c2b109ee59d67ed4506866c2
Eva Juez <xv4w04bu@students.codeinstitute.net>
2024-12-04 14:49:03 +0000

    Add Event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+PERMITTED = ((0, "Yes"), (1, "No"))
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="event_posts")
+    category = models.CharField(max_length=200)
+    # event_image = CloudinaryField('image', default='placeholder')
+    date = models.DateTimeField()
+    time = models.DateTimeField()
+    location = models.CharField(max_length=200)
+    description = models.TextField()
+    familiar = models.IntegerField(choices=PERMITTED, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
