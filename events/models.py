4f969e0888f1329a4e8c446e609229ad29b0565c
Eva Juez <xv4w04bu@students.codeinstitute.net>
2024-12-04 17:07:51 +0000

    Add Comment model and register in admin.py

--- events/models.py
+++ events/models.py
@@ -12,11 +12,19 @@ class Event(models.Model):
     category = models.CharField(max_length=200)
     # event_image = CloudinaryField('image', default='placeholder')
     date = models.DateTimeField()
-    time = models.DateTimeField()
     location = models.CharField(max_length=200)
     description = models.TextField()
+    excerpt = models.TextField()
     familiar = models.IntegerField(choices=PERMITTED, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    content = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
