d6d6f9ad913227c6c4a46a5a15208d8cc1e7f852
Eva Juez <xv4w04bu@students.codeinstitute.net>
2024-12-12 14:53:16 +0000

    Add comment form and success/errormessages

--- events/views.py
+++ events/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Event
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -29,6 +31,20 @@ def event_detail(request, slug):
     comments = event.comments.all().order_by("-created_on")
     comment_count = event.comments.filter(approved=True).count()
 
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.event = event
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+    )
+
+    comment_form = CommentForm()
+
     return render(
         request,
         "events/event_detail.html",
@@ -36,5 +52,6 @@ def event_detail(request, slug):
             "event": event,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
