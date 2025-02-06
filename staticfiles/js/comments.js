643c561e2b70eba9e594beb1ac341555c7e3ff6b
Eva Juez <xv4w04bu@students.codeinstitute.net>
2025-02-06 21:46:51 +0000

    Attempt collectstatic fix

--- staticfiles/js/comments.js
+++ staticfiles/js/comments.js
@@ -19,7 +19,7 @@ const deleteConfirm = document.getElementById("deleteConfirm");
 */
 for (let button of editButtons) {
   button.addEventListener("click", (e) => {
-    let commentId = e.target.getAttribute("comment_id");
+    let commentId = e.target.getAttribute("data-comment_id");
     let commentContent = document.getElementById(`comment${commentId}`).innerText;
     commentText.value = commentContent;
     submitButton.innerText = "Update";
@@ -39,8 +39,9 @@ for (let button of editButtons) {
 */
 for (let button of deleteButtons) {
   button.addEventListener("click", (e) => {
-    let commentId = e.target.getAttribute("comment_id");
-    deleteConfirm.href = `delete_comment/${commentId}`;
+    let commentId = e.target.getAttribute("data-comment_id");
+    let eventSlug = window.location.pathname.split("/")[2];  // Extract event slug from URL
+    deleteConfirm.href = `/events/${eventSlug}/delete_comment/${commentId}`;
     deleteModal.show();
   });
 }
